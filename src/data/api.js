bd6a999718bc32fba7c2eda4c9074600fd1896aa
seandolinar <sjjd199@gmail.com>
2019-09-13 12:52:57 -0400

    fixed UA string issue

--- src/data/api.js
+++ src/data/api.js
@@ -1,8 +1,9 @@
 const { JsonRpc } = require('eosjs');
 
-const fetch = require('node-fetch'); 
+// Safari fix for UA string
+let fetch = window.fetch || require('node-fetch');
 
-const rpc = new JsonRpc('https://api.eosnewyork.io', { fetch });
+const rpc = new JsonRpc('https://api.eosnewyork.io');
 
 export const apiGetInfo = () => rpc.get_info();
 
@@ -10,19 +11,4 @@ export const apiGetBlock = (blockId) => rpc.get_block(blockId);
 
 export const apiGetABI = (name) => rpc.get_abi(name);
 
-
-// apiGetInfo.then(res => {
-//     rpc.get_block(res.last_irreversible_block_num).then(res => {
-//         console.log(res)
-//     })
-// })
-
-
-
-// window.setTimeout(() => {
-//     const respBlock = rpc.get_block('04b3fb9510152702e437a1d549285412c5c210e7f29aa6ae14e7677fe45df1ea')
-
-
-
-// }, 1000)
 
